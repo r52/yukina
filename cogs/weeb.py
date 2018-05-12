acccbdecb4e3164b477349520cec6fbf2b590bdd
Simon <homer12345@gmail.com>
2018-05-12 00:33:25 -0700

    Refresh pixiv oauth if/when it expires

--- cogs/weeb.py
+++ cogs/weeb.py
@@ -9,29 +9,36 @@ from discord.ext import commands
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+
 class Weeb:
     def __init__(self, bot):
         self.bot = bot
         self.pixiv = None
         self.autotasks = {}
 
+        self._pixiv_login()
+
+    def _pixiv_login(self):
         if config['pixiv']['user']:
-            self.pixiv = AppPixivAPI(timeout=10)
+            if not self.pixiv:
+                self.pixiv = AppPixivAPI(timeout=10)
+
             try:
-                self.pixiv.login(config['pixiv']['user'], config['pixiv']['pass'])
+                self.pixiv.login(config['pixiv']['user'],
+                                 config['pixiv']['pass'])
             except Exception as e:
                 print("pixiv: Failed to login")
                 print(f"{e}")
                 self.pixiv = None
             else:
-                print(f"pixiv: Logged into pixiv as {config['pixiv']['user']}.")
+                print(
+                    f"pixiv: Logged into pixiv as {config['pixiv']['user']}.")
         else:
             print("pixiv: Unavailable")
 
     async def _autoimg_task(self, channel, *, timeout=30, nsfw=False):
         while True:
             await asyncio.sleep(timeout * 60)
-            print(f"Sending image to channel {channel.name}")
             await self._random_pixiv(channel, nsfw=nsfw)
 
     @commands.command()
@@ -58,7 +65,8 @@ class Weeb:
                 self.autotasks[ctx.channel.id].cancel()
                 del self.autotasks[ctx.channel.id]
 
-            self.autotasks[ctx.channel.id] = asyncio.get_event_loop().create_task(self._autoimg_task(ctx.channel, timeout=delay, nsfw=False))
+            self.autotasks[ctx.channel.id] = asyncio.get_event_loop().create_task(
+                self._autoimg_task(ctx.channel, timeout=delay, nsfw=False))
             await ctx.send(f"I will post images in this channel every {delay} minute(s)!")
 
     @commands.command()
@@ -78,23 +86,27 @@ class Weeb:
         if nsfw:
             ranking_modes = ['day_r18', 'week_r18']
 
-        if not nsfw and random.getrandbits(1):
-            json_result = self.pixiv.illust_recommended('illust')
-        else:
-            json_result = self.pixiv.illust_ranking(random.choice(ranking_modes))
+        while not json_result or 'illusts' not in json_result:
+            if not nsfw and random.getrandbits(1):
+                json_result = self.pixiv.illust_recommended('illust')
+            else:
+                json_result = self.pixiv.illust_ranking(
+                    random.choice(ranking_modes))
 
-        if not json_result:
-            print("pixiv: no illustration results")
-            return
+            if 'error' in json_result:
+                # Refresh oauth
+                self._pixiv_login()
 
         illust = random.choice(json_result.illusts)
         await self._post_pixiv(channel, illust)
 
     async def _post_pixiv(self, channel, illust, download=False):
         if download:
-            response = self.pixiv.requests_call('GET', illust.image_urls['large'], headers={ 'Referer': 'https://app-api.pixiv.net/' }, stream=True)
+            response = self.pixiv.requests_call('GET', illust.image_urls['large'], headers={
+                                                'Referer': 'https://app-api.pixiv.net/'}, stream=True)
             print(illust)
-            img = discord.File(response.raw, os.path.basename(illust.image_urls['large']))
+            img = discord.File(response.raw, os.path.basename(
+                illust.image_urls['large']))
             await channel.send(f"https://pixiv.net/i/{illust.id}", file=img)
             del response
         else:
@@ -106,6 +118,7 @@ class Weeb:
 
         if "this is my fight" in message.content:
             await message.channel.send('No, Senpai. This is our fight!')
+
 
 def setup(bot):
     bot.add_cog(Weeb(bot))
